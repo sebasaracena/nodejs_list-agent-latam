0ea734980bf5458245212d3f7d68295d188b4b24
Sebastian Aracena Aguirre <sebastian.mdres.aracena@gmail.com>
2023-04-22 20:49:11 +1000

    change name route

--- src/api/nodejs_list/hits.routes.js
+++ src/api/nodejs_list/hits.routes.js
@@ -4,6 +4,6 @@ const { hitsController } = require("./hits.controller");
 //to search and view the diferents data have the view;
 router.post("/list_hits", hitsController.hitsList);
 router.delete("/delete_hits/:id", hitsController.deleteHit);
-router.get("/get_data_hits/", hitsController.serverConectHits);
+router.get("/insert_data_hits/", hitsController.serverConectHits);
 
 module.exports = router;
